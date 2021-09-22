e4cdf33077eeeeae842453f86b943bd1fef7453d
Mitha <paramithatm@gmail.com>
2021-09-23 00:27:52 +0700

    remove switch

--- CamExplore/AVCameraViewSetup.swift
+++ CamExplore/AVCameraViewSetup.swift
@@ -11,16 +11,10 @@ extension AVFoundationCameraViewController {
     
     func setupView(){
        view.backgroundColor = .black
-       view.addSubview(switchCameraButton)
        view.addSubview(captureImageButton)
        view.addSubview(dismissButton)
        
        NSLayoutConstraint.activate([
-           switchCameraButton.widthAnchor.constraint(equalToConstant: 50),
-           switchCameraButton.heightAnchor.constraint(equalToConstant: 50),
-           switchCameraButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
-           switchCameraButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-           
            captureImageButton.widthAnchor.constraint(equalToConstant: 50),
            captureImageButton.heightAnchor.constraint(equalToConstant: 50),
            captureImageButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
@@ -32,7 +26,6 @@ extension AVFoundationCameraViewController {
            dismissButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
        ])
        
-       switchCameraButton.addTarget(self, action: #selector(switchCamera(_:)), for: .touchUpInside)
        captureImageButton.addTarget(self, action: #selector(captureImage(_:)), for: .touchUpInside)
        dismissButton.addTarget(self, action: #selector(dismissVc(_:)), for: .touchUpInside)
     }
